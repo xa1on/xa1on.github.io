2592ad55311063684435752dd8250334cc4a777c
cli <82491956+xa1on@users.noreply.github.com>
2026-07-04 13:24:51 -0700

    feat: add sawan, joe, me, jaysa, buddybox

--- js/buddies.js
+++ js/buddies.js
@@ -1,4 +1,8 @@
 export const buddies = [
+    'sawansri.com.png',
+    'joewang.me.png',
+    'chenghao.li.png',
+    'jaysa.net.png',
     'decal.ocf.berkeley.edu.png',
-    'ocf.berkeley.edu.png'
+    'ocf.berkeley.edu.png',
 ];
