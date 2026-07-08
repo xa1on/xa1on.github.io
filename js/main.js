06456f88d9b8e0ed5d975d0bf10724859116b6d1
xa1on <xalon.dzn@gmail.com>
2026-07-08 00:39:02 -0700

    feat: more ascii detailing for MOTD

--- js/main.js
+++ js/main.js
@@ -14,25 +14,25 @@ const shell = new Shell({
     const asciiArt = `<span class="color-dim red">
 
        ░░░░    ░  ░░ ░░░░   ░░░                ░░                             </span><span class="red">
-      ░░░     ░  ░░░░░                    ░         ░░                        
+      ░░░     ░  ░░░░░                    <span class="color-dim red">▄</span>         ░░                        
      ░░░<span class="color-dim red"> ░</span>   ▒  ▒  ▒▒                               ░  ░                      
     ▒▒▒▒       ▒▒▒▒                   </span>▓█</span><span class="red">          ▒                           
-    ▒▒▒<span class="color-dim red">  ▒</span>          ▒▒               </span>█  █</span><span class="red">               ▒                     
-   ▒ ▒<span class="color-dim red"> ▒▒▒▒</span>        ▒▒               </span>█    █</span><span class="red">        ▒                           
-   ▓▓▓  ▓          ▓            </span>   █ ▓▓▓  █</span><span class="color-dim red">█▓             ▓  ▓                
+    ▒▒▒<span class="color-dim red">  ▒</span>          <span class="color-dim red">▄▄</span>               </span>█  █</span><span class="red">               ▒                     
+   ▒ ▒<span class="color-dim red"> ▒▒▒▒</span>        ▒▒               </span>█    █</span><span class="red">        ▄                           
+   ▓▓▓  ▓          ▄            </span>   █ ▓▓▓  █</span><span class="color-dim red">█▓             ▓  ▓                
   ▓ ▓ ▓  ▓  ▓▓  ▓▓    <span class="color-dim red">▒</span>   ▓</span>█<span class="red">█ ▓</span> ▓▞▀▀▀▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▛▀</span><span class="red">                       
   ▓ ▓            ▓       ██ ▓ ▓</span>██     ▓█ ▓▓ █ ▓     ▓</span><span class="red">                         
   ▓▓▓            ▓  <span class="color-dim red">▒</span>   █ ▓  </span>█<span class="red">█</span>██   ▄▞██▓█   ████▓ █</span><span class="red">                          
    ▓▓   ░          <span class="color-dim red">▒ ▒</span> </span>█<span class="red">▓ ▓▓▓</span>  ▓   ▟     ▚     ██████</span><span class="red">        <span class="color-dim red">▒</span>          </span> | <span class="blue">arch</span>4ic</span><span class="red">
   ▓▓▓   ░      ▓▒    <span class="color-dim red">▒</span></span>█▓<span class="red">  █▓</span> ▓█  ▓▄█      ██▓  ██ ████</span><span class="red">▒                       
   ▓▓▓   ░       ▓░    ██ ▓</span> █ █ █▀  ▓█▓ ▄<span class="red">▓</span>██ ███▓█ █  <span class="red">▓</span> █</span><span class="red">                      
-  ▓ ▓           ▓░      </span>█ ▓ <span class="red">█</span>█ <span class="red">▓</span>    ▄   ▚   █  <span class="red">▓</span> █    <span class="red">▓</span>█</span><span class="red">                      
-  ▓ ▓▓▓▓▓      ▓▒    ▓▓ </span>█<span class="red">█▓</span>█ ▄▄▄▄▄▓▓▄▄▓▄▄▓▓▓▓▄▄▄▄<span class="red">▓</span>███<span class="red">▓                       
-    ▓▓  ▓       ▓░ <span class="color-dim red">░</span>      </span>█   <span class="red">▓</span>█    ▀▀▀▀▀▀    ██   █<span class="red">   ▓  ▓          ▓  ▓     
+  ▓ ▓           ▓░      </span>█ ▓ <span class="red">█</span>█ <span class="red">▄</span>    ▄   ▚   █  <span class="red">▓</span> █    <span class="red">▓</span>█</span><span class="red">                      
+  ▓ ▓▓▓▓▓      ▓▒    ▞▀ </span>█<span class="red">█▓</span>█ ▄▄▄▄▄▓▓▄▄▓▄▄▓▓▓▓▄▄▄▄<span class="red">▓</span>███<span class="red">▓                       
+    ▓▓  ▓       ▓░ <span class="color-dim red">░</span>      </span>█   <span class="red">▓</span>█    ▀▀▀▀▀▀    ██   █<span class="red">   ▓  <span class="color-dim red">▄</span>          ▓  ▓     
    ▒ ▒ ▒▒▒▒▒   ░  <span class="color-dim red">▒▒▒</span>    </span>█    █                █ <span class="red">▓</span>  █<span class="red">     ▒        ▒▒▒  ▒     
-    ▒▒▒  ▒ ▒      <span class="color-dim red">▒▒▒▒</span>  </span>█    █               <span class="red">   █▓  ▓</span>█<span class="red">  ▒▒          ▒  ▒      
+    ▒▒▒  ▒ ▒      <span class="color-dim red">▒▒▒▒</span>  </span>█    █               <span class="red">   █▓  ▄</span>█<span class="red">  ▒▒          ▒  ▒      
     ▒ ▒▒    ▒          </span>█▄▄▄▄▞               <span class="red">░</span>    ▚▄▄▄█<span class="red">               ▒▒      
-     ░ ░ ░   ░   ░    ░                                         ░     ░       
+     ░ ░ ░   ░   ░    <span class="color-dim red">▄</span >                                         ░     ░       
       ░ ░     ░         ░                                      ░     ░ ░      </span><span class="color-dim red">
        ░ ░░    ░            ░░░                ░░░           ░░    ░░ ░       </span>
 
