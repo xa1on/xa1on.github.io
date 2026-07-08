31f751edfa7b1dd517b252c36a6211c8dc15c570
xa1on <xalon.dzn@gmail.com>
2026-07-07 20:40:14 -0700

    feat: auto generated buddies.js via geneate_vfs

--- js/buddies.js
+++
@@ -1,8 +0,0 @@
-export const buddies = [
-    'sawansri.com.png',
-    'joewang.me.png',
-    'chenghao.li.png',
-    'jaysa.net.png',
-    'decal.ocf.berkeley.edu.png',
-    'ocf.berkeley.edu.png',
-];
--- scripts/generate_vfs.js
+++ scripts/generate_vfs.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const { buildVfsTree, generateManifestContent, generateCommandsIndexContent } = require('./cache_buster.js');
+const { buildVfsTree, generateManifestContent, generateCommandsIndexContent, generateBuddiesListContent } = require('./cache_buster.js');
 
 async function main() {
-  // 1. Generate Virtual File System Manifest
-  const vfsTree = buildVfsTree('.');
-  const manifestContent = generateManifestContent(vfsTree);
-
   const destDir = path.join('js');
   if (!fs.existsSync(destDir)) {
     fs.mkdirSync(destDir, { recursive: true });
   }
-  fs.writeFileSync(path.join(destDir, 'fs_manifest.js'), manifestContent, 'utf8');
-  console.log('Virtual File System manifest generated successfully in js/fs_manifest.js');
+
+  // 1. Generate Buddies List (must be done first as commands import it)
+  const buddiesContent = generateBuddiesListContent();
+  fs.writeFileSync(path.join(destDir, 'buddies.js'), buddiesContent, 'utf8');
+  console.log('Buddies list generated successfully in js/buddies.js');
 
   // 2. Generate Commands Index
   const indexContent = await generateCommandsIndexContent();
@@ -22,6 +21,12 @@ async function main() {
   }
   fs.writeFileSync(path.join(commandsDir, 'index.js'), indexContent, 'utf8');
   console.log('Commands index generated successfully in js/commands/index.js');
+
+  // 3. Generate Virtual File System Manifest
+  const vfsTree = buildVfsTree('.');
+  const manifestContent = generateManifestContent(vfsTree);
+  fs.writeFileSync(path.join(destDir, 'fs_manifest.js'), manifestContent, 'utf8');
+  console.log('Virtual File System manifest generated successfully in js/fs_manifest.js');
 }
 
 main().catch(console.error);
